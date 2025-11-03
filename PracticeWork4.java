5d65d0aa84359806062472ab3847a1cf7609178d
Ivan Khlopchik <vanilin1403@gmail.com>
2025-11-03 15:57:12 +0200

    Recoded
    Now its currect variant

--- PracticeWork4.java
+++ PracticeWork4.java
@@ -5,17 +5,25 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         //  Завдання 1
-        double x, a, b, f;
+        System.out.println("Завдання 1:");
         System.out.print("Введіть x: ");
-        x = sc.nextDouble();
+        double x = sc.nextDouble();
         System.out.print("Введіть a: ");
-        a = sc.nextDouble();
+        double a = sc.nextDouble();
         System.out.print("Введіть b: ");
-        b = sc.nextDouble();
+        double b = sc.nextDouble();
 
-        if (x >= 1 && x < 3) f = 9 / (a * x);
-        else if (x == 3) f = a * x * x + x + b;
-        else {
+        double f;
+        if (x >= 1) {
+            if (x < 3) {
+                f = 9 / (a * x);
+            } else if (x == 3) {
+                f = a * x * x + x + b;
+            } else {
+                System.out.println("x поза межами області визначення");
+                f = Double.NaN;
+            }
+        } else {
             System.out.println("x поза межами області визначення");
             f = Double.NaN;
         }
@@ -23,24 +31,41 @@ public class Main {
 
 
         //  Завдання 2
+        System.out.println("\nЗавдання 2:");
         Random rand = new Random();
         int number = rand.nextInt(10) + 1;
         int guess, attempts = 0;
+
         do {
             System.out.print("Введіть число: ");
             guess = sc.nextInt();
             attempts++;
             if (guess > number) System.out.println("Занадто багато");
             else if (guess < number) System.out.println("Занадто мало");
-            else System.out.println("Вірно!");
         } while (guess != number);
-        System.out.println("Кількість спроб: " + attempts);
+        System.out.println("Вірно! Спроб: " + attempts);
+
+        System.out.println("Гра ще раз! :");
+        number = rand.nextInt(100) + 1;
+        attempts = 0;
+        guess = -1;
+        while (guess != number) {
+            System.out.print("Введіть число: ");
+            guess = sc.nextInt();
+            attempts++;
+            if (guess > number) System.out.println("Занадто багато");
+            else if (guess < number) System.out.println("Занадто мало");
+        }
+        System.out.println("Вірно! Спроб: " + attempts);
 
 
         //  Завдання 3
+        System.out.println("\nЗавдання 3:");
         System.out.print("Введіть N: ");
         int N = sc.nextInt();
-        System.out.println("Прості числа від 1 до " + N + ":");
+        System.out.print("Прості числа від 1 до " + N + ": ");
+
+        boolean first = true;
         for (int i = 2; i <= N; i++) {
             boolean prime = true;
             for (int j = 2; j <= Math.sqrt(i); j++) {
@@ -49,7 +74,12 @@ public class Main {
                     break;
                 }
             }
-            if (prime) System.out.println(i + " ");
+            if (prime) {
+                if (!first) System.out.print(", ");
+                System.out.print(i);
+                first = false;
+            }
         }
+        System.out.println(".");
     }
 }
